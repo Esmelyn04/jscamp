c931d91d98d7fac51ad34501757d31793d5060d5
Jose <esmelyn04@gmail.com>
2026-06-02 19:51:47 -0400

    update buttons event listener.

--- 00-html-css/script.js
+++ 00-html-css/script.js
@@ -1,4 +1,5 @@
-// querySelector selected only the first button with this id 
+// querySelector selected only the first button with this id
+
 // const boton = document.querySelector('#boton-importante')
 // console.log(boton)
 // 
@@ -10,14 +11,30 @@
 //     })
 // }
 
-const botones = document.querySelectorAll('.button-apply-job')
-// devuelve un NodeList (array-like) con todos los botones que encuentra
-// o una lista vacia [] si no encuentra ninguno
 
-botones.forEach(boton => {
-    boton.addEventListener("click", () => {
-        boton.textContent = "applied"
-        boton.classList.add('is-applied')
-        boton.disabled = true
-    } )
+// const botones = document.querySelectorAll('.button-apply-job')
+// // devuelve un NodeList (array-like) con todos los botones que encuentra
+// // o una lista vacia [] si no encuentra ninguno
+// 
+// botones.forEach(boton => {
+//     boton.addEventListener("click", () => {
+//         boton.textContent = "applied"
+//         boton.classList.add('is-applied')
+//         boton.disabled = true
+//     } )
+// })
+
+
+const jobsListingSection = document.querySelector('.jobs-listings')
+
+jobsListingSection.addEventListener('click', function(event) {
+    console.log('click')
+    const element = event.target
+
+    if(element.classList.contains('button-apply-job')) {
+        element.textContent = 'Applied'
+        element.classList.add('is-applied')
+        element.disabled = true
+    }
+    
 })

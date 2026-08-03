6528bdc9a0727c885abcfa862ce75dd577e35543
Jose <esmelyn04@gmail.com>
2026-08-03 19:08:29 -0400

    Add FavoritesContext and provider; integrate into main application for managing favorite jobs

--- 03-router-and-zustand/src/main.jsx
+++ 03-router-and-zustand/src/main.jsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import { AuthProvider } from './context/AuthContext.jsx'
+import { FavoritesProvider } from './context/FavContext.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AuthProvider>
-      <App />
+      <FavoritesProvider>
+        <App />
+      </FavoritesProvider>
     </AuthProvider>
   </BrowserRouter>,
 )
